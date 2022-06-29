6885c9da471d18273020f8516069b0eb597e32d5
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-06-29 13:23:20 -0400

    Avance de ejercicio asincrónico primera clase - TaskList

---
+++ c1_js/js/scripts.js
@@ -0,0 +1,18 @@
+// Etiquetas HTML
+const btn_agregar = document.querySelector(".boton-agregar");
+const btn_editar = document.querySelector(".boton-editar");
+const btn_remover = document.querySelector(".boton-remover");
+
+
+// Eventos de botón
+btn_agregar.addEventListener('click', function(){
+    console.log("Agregar");
+});
+
+btn_editar.addEventListener('click', function(){
+    console.log("Editar");
+});
+
+btn_remover.addEventListener('click', function(){
+    console.log("Remover");
+});
