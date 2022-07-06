6cd067f1c3197ea4953986e8e0a9a26955981dff
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-07-06 13:00:55 -0400

    Se añade funcionalidad para el botón editar y eliminar inputs

--- c1_js/js/scripts.js
+++ c1_js/js/scripts.js
@@ -1,48 +1,82 @@
+
 // Etiquetas HTML
 const btn_add = document.querySelector(".boton-agregar");
-const btn_edit = document.querySelector(".boton-editar");
-const btn_delete = document.querySelector(".boton-remover");
 const input = document.querySelector(".input");
-const item = document.querySelector(".item");
-
-
-// Eventos de botón
-btn_add.addEventListener('click', function () {
-    if(input.value.length === 0){
-        alert("Ingrese un valor");
-    }else{
-        const item = new Item(input.value.trim());
-    }
-});
-
-btn_edit.addEventListener('click', function (e) {
-    document.querySelector(".item-input").removeAttribute('disabled');
-});
-
-btn_delete.addEventListener('click', function () {
-    console.log("Remover");
-});
-
+const container = document.querySelector(".container");
+// Clase Item
 class Item {
     constructor(newTask) {
-        this.crearDiv(newTask);
+        this.createDiv(newTask);
     }
 
-    crearDiv(newTask) {
-        console.log(newTask);
-        let divItem = document.createElement("div");
-        divItem.classList.add('input-group', 'mb-3');
-
-        let inputItem = document.createElement("input");
+    createDiv(newTask) {
+        // creando el div contenedor
+        const divItem = document.createElement("div");
+        divItem.classList.add('input-group', 'item', 'mb-3');
+        // creando el input
+        const inputItem = document.createElement("input");
         inputItem.setAttribute('type', 'text');
         inputItem.setAttribute('disabled', true);
         inputItem.classList.add('item-input', 'form-control', 'fst-italic');
         inputItem.value = newTask;
+        divItem.appendChild(inputItem);
+        // creando los botones
+        const btn_edit = document.createElement("button");
+        btn_edit.classList.add('btn', 'btn-outline-success', 'boton-editar');
+        btn_edit.innerHTML = `<i class="bi bi-lock"></i>`;
+        // agrego el evento y la función a ejecutar cuando se haga click en el botón
+        btn_edit.addEventListener('click', function () {changeStateBtnEdit(inputItem, btn_edit)}, false);
+        divItem.appendChild(btn_edit);
+        const btn_delete = document.createElement("button");
+        btn_delete.classList.add('btn', 'btn-danger', 'boton-remover');
+        btn_delete.innerHTML = `<i class="bi bi-trash3"></i>`;
+        // agrego el evento y la función para el boton eliminar
+        btn_delete.addEventListener('click', function () {deleteTask(divItem)}, false);
+        divItem.appendChild(btn_delete);
+        // Agrego todo
+        container.appendChild(divItem);
+    }
+}
 
-        item.appendChild(divItem).appendChild(inputItem);
-        divItem.insertAdjacentHTML('beforeend', ` <button class="input-group-text boton-editar"><i class="bi bi-lock"></i></button>
-        <button class="input-group-text boton-remover btn btn-danger">
-            <i class="bi bi-trash3"></i>
-        </button>`);
+// Eventos de botón
+btn_add.addEventListener('click', function () {
+    checkInput(input);
+});
+
+/**
+ * Función para modificar el estado del botón editar dependiendo si está habilitado o no
+ * @param {*} inputItem input:text a modificar
+ * @param {*} btn_edit el boton para cambiarle los iconos
+ */
+function changeStateBtnEdit(inputItem, btn_edit) {
+    if (inputItem.disabled) {
+        inputItem.disabled = false;
+        btn_edit.innerHTML = `<i class="bi bi-unlock"></i>`;
+    } else {
+        inputItem.disabled = true;
+        btn_edit.innerHTML = `<i class="bi bi-lock"></i>`;
+    }
+}
+
+/**
+ * Función para chequear si el input está vacío o no
+ * @param {*} task input:text de la tarea a agregar
+ */
+function checkInput(task) {
+    if (task.value.length === 0) {
+        alert("Ingrese un valor");
+    } else {
+        const item = new Item(task.value.trim());
+        task.value = "";
+    }
+}
+
+/**
+ * Función para eliminar una tarea específica de la lista
+ * @param {*} divItem contenedor principal donde está agregado el input
+ */
+function deleteTask(divItem) {
+    if (divItem.classList.contains('item')) {
+        divItem.remove();
     }
 }
